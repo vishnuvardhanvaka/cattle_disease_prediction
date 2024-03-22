5709e43900f5521c0633645c07e5739fe9b99277
vishnu <vishnu.vaka@solomonsint.com>
2024-03-22 18:35:18 +0530

    changed the host in main.py

--- cdp_backend/main.py
+++ cdp_backend/main.py
@@ -3,4 +3,4 @@ from os import getenv
 
 if __name__=='__main__':
     port=int(getenv("PORT",8000))
-    uvicorn.run('app.api:app',host='127.0.0.1',port=port,reload=True)
+    uvicorn.run('app.api:app',host='0.0.0.0',port=port,reload=True)
